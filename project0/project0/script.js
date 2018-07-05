dc399f1c740d3512e163349cea1e2a22c9c6244a
Tony Yu (tonyu2) <tonyu2@cisco.com>
2018-07-05 19:18:36 -0400

    [task] enable delete To-do item

--- project0/project0/script.js
+++ project0/project0/script.js
@@ -9,17 +9,40 @@ const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
+function increment(str){return parseInt(str) + 1};
+
+function decrement(str){return parseInt(str) - 1};
+
 function newTodo() {
   const count = parseInt(itemCountSpan.textContent)
   const unchecked_count = parseInt(uncheckedCountSpan.textContent)
   const div = document.createElement("div");
   const li = document.createElement("li");
   const checkbox = document.createElement("input");
+  const deleteButton = document.createElement("button");
+  deleteButton.className = classNames.TODO_DELETE;
+  deleteButton.textContent = "Delete!";
+  deleteButton.onclick = function() {
+  	div.remove();
+  	if (!checkbox.checked){
+  		uncheckedCountSpan.textContent = decrement(uncheckedCountSpan.textContent);
+  	}
+  	itemCountSpan.textContent = decrement(itemCountSpan.textContent);
+  } 
   checkbox.type = 'checkbox';
   checkbox.className = classNames.TODO_CHECKBOX;
+  checkbox.onclick = function() {
+  	if (this.checked) {
+  		uncheckedCountSpan.textContent = decrement(uncheckedCountSpan.textContent);
+  	}
+  	else {
+  		uncheckedCountSpan.textContent = increment(uncheckedCountSpan.textContent);
+  	}
+  }
   li.className = classNames.TODO_ITEM;
   li.textContent = 'To Do Item: ' + count;
-  div.appendChild(checkbox);
+  li.appendChild(checkbox);
+  li.appendChild(deleteButton);
   div.appendChild(li);
   list.appendChild(div);
   // increment the todo count
